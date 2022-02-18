291e7cb6887ad0f244ca3c7eafff8aaf10462e3b
jollaman999 <admin@jollaman999.com>
2022-02-18 22:05:26 +0900

    Create Pattern : Singleton - Implement the safest way to prevent breaking singleton using enum
    - Even if enum is serialized and deserialized, the same instance is returned.

--- src/main/java/com.create.singleton/Main.java
+++ src/main/java/com.create.singleton/Main.java
@@ -1,17 +1,18 @@
 package com.create.singleton;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         Settings settings = Settings.INSTANCE;
-
         Settings settings1 = null;
-        Constructor<?>[] declaredConstructors = Settings.class.getDeclaredConstructors();
-        for (Constructor<?> constructor : declaredConstructors) {
-            constructor.setAccessible(true);
-            settings1 = (Settings)  constructor.newInstance("INSTANCE");
+
+        try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))) {
+            out.writeObject(settings);
+        }
+
+        try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))) {
+            settings1 = (Settings) in.readObject();
         }
 
         System.out.println(settings == settings1);
