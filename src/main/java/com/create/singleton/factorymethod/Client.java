e15a710e27d1d123dc75d58464ef8154a5f74ed9
jollaman999 <admin@jollaman999.com>
2022-02-18 22:10:20 +0900

    Create Pattern : Factory Method - Before apply

---
+++ src/main/java/com/create/singleton/factorymethod/Client.java
@@ -0,0 +1,14 @@
+
+package com.create.singleton.factorymethod;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Ship whiteship = ShipFactory.orderShip("Whiteship", "keesun@mail.com");
+        System.out.println(whiteship);
+
+        Ship blackship = ShipFactory.orderShip("Blackship", "keesun@mail.com");
+        System.out.println(blackship);
+    }
+
+}
