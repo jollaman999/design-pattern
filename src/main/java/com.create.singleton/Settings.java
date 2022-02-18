c4a584c0ad160eaad83c7b7241447b7a01a0026e
jollaman999 <admin@jollaman999.com>
2022-02-18 21:57:03 +0900

    Create Pattern: Singleton - Make Thread Safe using synchronized keyword

--- src/main/java/com.create.singleton/Settings.java
+++ src/main/java/com.create.singleton/Settings.java
@@ -1,13 +1,13 @@
 package com.create.singleton;
 
 public class Settings {
-    private static Settings instance;
+    private static Settings instance = new Settings();
 
     private Settings() {
 
     }
 
-    public static Settings getInstance() {
+    public static synchronized Settings getInstance() {
         if (instance == null) {
             instance = new Settings();
         }
