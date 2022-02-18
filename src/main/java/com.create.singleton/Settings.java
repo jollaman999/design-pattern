8bab6731d3c64a13f9d471eaf89a4742a385c4e4
jollaman999 <admin@jollaman999.com>
2022-02-18 21:57:44 +0900

    Create Pattern: Singleton - Make Thread Safe using eager initialization

--- src/main/java/com.create.singleton/Settings.java
+++ src/main/java/com.create.singleton/Settings.java
@@ -1,17 +1,13 @@
 package com.create.singleton;
 
 public class Settings {
-    private static Settings instance = new Settings();
+    private static final Settings INSTANCE = new Settings();
 
     private Settings() {
 
     }
 
-    public static synchronized Settings getInstance() {
-        if (instance == null) {
-            instance = new Settings();
-        }
-
-        return instance;
+    public static Settings getInstance() {
+        return INSTANCE;
     }
 }
